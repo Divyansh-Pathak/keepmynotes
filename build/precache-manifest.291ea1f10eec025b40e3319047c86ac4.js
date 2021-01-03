524b65ee7bd94a82fd6807b562abb1b4d3b6d5bd
Divyansh <pathak.divyansh0029@gmail.com>
2021-01-03 13:30:52 +0530

    Ingnore node Modules2

---
+++ build/precache-manifest.291ea1f10eec025b40e3319047c86ac4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2d81f73a39786b55b13bc4edb967ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f1ec43c821f2d9c85b",
+    "url": "/static/js/2.e211f536.chunk.js"
+  },
+  {
+    "revision": "9bfbbda15f65ccfefbf1",
+    "url": "/static/js/main.fcde4c6f.chunk.js"
+  },
+  {
+    "revision": "b1cfa85f861ca69c3b05",
+    "url": "/static/js/runtime-main.941946b3.js"
+  }
+]);
